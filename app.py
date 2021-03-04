c9a1327608f666254176d2d17c998612df08144d
chuckloveall <chuckloveall@gmail.com>
2021-03-03 18:39:40 -0800

    updated .ipynb file

---
+++ app.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///../Resources/chinook.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to the tables
